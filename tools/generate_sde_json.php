7be1af05d359c3120260efed09ddf6dc596db155
Leon Jacobs <leonja511@gmail.com>
2016-02-10 06:15:47 +0200

    Update to yc118-2-116998

--- tools/generate_sde_json.php
+++ tools/generate_sde_json.php
@@ -19,7 +19,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-$sde_name = 'yc118-1-116645';
+$sde_name = 'yc118-2-116998';
 
 // Prepare the data that we want represented as JSON
 $data = [
